0088e423c30552f1f1708f5612fffca6e27855e3
Chris Carini <6374067+ChrisCarini@users.noreply.github.com>
2025-08-04 21:29:22 -0700

    Bump org.jetbrains.intellij.platform:intellij-platform-gradle-plugin (https://github.com/ChrisCarini/github-repo-files-sync/pull/272) (#542)
    Bumps org.jetbrains.intellij.platform:intellij-platform-gradle-plugin from 2.6.0 to 2.7.0.
    
    ---
    updated-dependencies:
    - dependency-name: org.jetbrains.intellij.platform:intellij-platform-gradle-plugin
      dependency-version: 2.7.0
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.intellij.platform:intellij-platform-gradle-plugin:2.6.0")
+    implementation("org.jetbrains.intellij.platform:intellij-platform-gradle-plugin:2.7.0")
     implementation("org.jetbrains.intellij.plugins:gradle-changelog-plugin:2.2.1")
     implementation("com.dorongold.plugins:task-tree:4.0.1") // provides `taskTree` task (e.g. `./gradlew build taskTree`; docs: https://github.com/dorongold/gradle-task-tree)
 }
